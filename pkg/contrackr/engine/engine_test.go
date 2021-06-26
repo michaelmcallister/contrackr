60a1d2db7dd52f8b73f9aa676afcf87d91695708
Michael McAllister <git@sknk.ws>
2021-06-26 21:14:05 +1000

    Add test for engine to ensure it issues block requests.

---
+++ pkg/contrackr/engine/engine_test.go
@@ -0,0 +1,92 @@
+package engine
+
+import (
+	"net"
+	"sync"
+	"sync/atomic"
+	"testing"
+)
+
+// fakeBlocker implements the BlockCloser interface.
+type fakeBlocker struct {
+	blockCalled uint64
+}
+
+// Block will track whether it has been called, returning a nil error always.
+func (fb *fakeBlocker) Block(_ *net.IP) error {
+	atomic.AddUint64(&fb.blockCalled, 1)
+	return nil
+}
+
+// Close always returns nil.
+func (fb *fakeBlocker) Close() error {
+	return nil
+}
+
+// fakeTracker implements the Adder interface.
+type fakeTracker struct {
+	tc chan *TrackerEntry
+}
+
+// Add is a no-op.
+func (ft *fakeTracker) Add(_ *Connection) {}
+
+// PortScanners returns the channel that callers can recieve as soon as a
+// "port scanner" is identified.
+func (ft *fakeTracker) PortScanners() chan *TrackerEntry {
+	return ft.tc
+}
+
+// fakeCapturer implements the CaptureCloser interface.
+type fakeCapturer struct {
+	captureChan chan *Connection
+}
+
+// Capture returns the channel that contains TCP SYN connections as soon as
+// they are parsed.
+func (fc *fakeCapturer) Capture() chan *Connection {
+	return fc.captureChan
+}
+
+// Close is a no-op.
+func (fc *fakeCapturer) Close() error {
+	return nil
+}
+
+func TestEngineBlocksPortScans(t *testing.T) {
+	// Entries added to this channel are considered "port scanners"
+	portscanners := make(chan *TrackerEntry)
+	fakeBlocker := &fakeBlocker{}
+	fakeEngine := &Engine{
+		capturer: &fakeCapturer{captureChan: make(chan *Connection)},
+		firewall: fakeBlocker,
+		tracker:  &fakeTracker{tc: portscanners},
+	}
+	var wg sync.WaitGroup
+
+	// Run the engine.
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		fakeEngine.Run()
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		defer fakeEngine.Close()
+		// Send an entry to the portscanner channel.
+		srcIP, dstIP := net.ParseIP("192.168.86.158"), net.ParseIP("192.168.86.191")
+		portscanners <- &TrackerEntry{
+			DstIP: &srcIP,
+			SrcIP: &dstIP,
+			Ports: map[int]int{1992: 1, 7: 1, 9: 1},
+		}
+	}()
+	wg.Wait()
+
+	// Test that the engine correctly sent a block request for this entry.
+	if atomic.LoadUint64(&fakeBlocker.blockCalled) != 1 {
+		t.Errorf("Expected block to be called once but was called %d times", fakeBlocker.blockCalled)
+	}
+}
