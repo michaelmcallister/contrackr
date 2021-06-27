6add630c089d7ec1b0b40b68faca6e84838c2a58
Michael McAllister <git@sknk.ws>
2021-06-27 16:01:57 +1000

    Add tests for iptables.

--- pkg/contrackr/engine/iptables.go
+++ pkg/contrackr/engine/iptables.go
@@ -22,7 +22,16 @@ const (
 
 // BLocker contains the methods for Blocking IP addresses.
 type Blocker struct {
-	iptables, ip6tables *iptables.IPTables
+	ip4tables, ip6tables iptable
+}
+
+type iptable interface {
+	ChainExists(string, string) (bool, error)
+	NewChain(string, string) error
+	Insert(string, string, int, ...string) error
+	AppendUnique(string, string, ...string) error
+	DeleteIfExists(string, string, ...string) error
+	ClearAndDeleteChain(string, string) error
 }
 
 var (
@@ -41,23 +50,28 @@ func newBlocker() (*Blocker, error) {
 	if err != nil {
 		return nil, err
 	}
-	for _, i := range []*iptables.IPTables{v4, v6} {
+	b := &Blocker{ip4tables: v4, ip6tables: v6}
+	return b, b.init()
+}
+
+func (b *Blocker) init() error {
+	for _, i := range []iptable{b.ip4tables, b.ip6tables} {
 		// Create our own chain if it doesn't exist.
 		ok, err := i.ChainExists(defaultTable, contrackrChain)
 		if err != nil {
-			return nil, err
+			return err
 		}
 		if !ok {
 			if err := i.NewChain(defaultTable, contrackrChain); err != nil {
-				return nil, err
+				return err
 			}
 		}
 		// Add an entry to INPUT to jump to our chain.
 		if err := i.Insert(defaultTable, inputChain, 1, jumpRuleSpec...); err != nil {
-			return nil, err
+			return err
 		}
 	}
-	return &Blocker{iptables: v4, ip6tables: v6}, nil
+	return nil
 }
 
 // Block will take the IP Address v and add an entry to the host firewall.
@@ -67,13 +81,13 @@ func (b *Blocker) Block(v *net.IP) error {
 	if isV6 {
 		return b.ip6tables.AppendUnique(defaultTable, contrackrChain, rule...)
 	}
-	return b.iptables.AppendUnique(defaultTable, contrackrChain, rule...)
+	return b.ip4tables.AppendUnique(defaultTable, contrackrChain, rule...)
 }
 
 // Close will cleanup the firewall rules that were created during instantiation.
 func (b *Blocker) Close() error {
 	var closeErr error
-	for _, i := range []*iptables.IPTables{b.iptables, b.ip6tables} {
+	for _, i := range []iptable{b.ip4tables, b.ip6tables} {
 		if err := i.DeleteIfExists(defaultTable, inputChain, jumpRuleSpec...); err != nil {
 			closeErr = fmt.Errorf("deleting jump rule: %v: %w", err, closeErr)
 		}
---
+++ pkg/contrackr/engine/iptables_test.go
@@ -0,0 +1,118 @@
+package engine
+
+import (
+	"fmt"
+	"net"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+// fakeIptables keeps a list of methods and the order they were executed in.
+type fakeIptables struct {
+	commandsExecuted []string
+}
+
+func (fi *fakeIptables) ChainExists(table, chain string) (bool, error) {
+	fi.commandsExecuted = append(fi.commandsExecuted, fmt.Sprintf("ChainExists(%s, %s)", table, chain))
+	return false, nil
+}
+
+func (fi *fakeIptables) NewChain(table, chain string) error {
+	fi.commandsExecuted = append(fi.commandsExecuted, fmt.Sprintf("NewChain(%s, %s)", table, chain))
+	return nil
+}
+
+func (fi *fakeIptables) Insert(table, chain string, pos int, rulespec ...string) error {
+	m := fmt.Sprintf("Insert(%s, %s, %d, %v)", table, chain, pos, rulespec)
+	fi.commandsExecuted = append(fi.commandsExecuted, m)
+	return nil
+}
+
+func (fi *fakeIptables) AppendUnique(table, chain string, rulespec ...string) error {
+	m := fmt.Sprintf("AppendUnique(%s, %s, %v)", table, chain, rulespec)
+	fi.commandsExecuted = append(fi.commandsExecuted, m)
+	return nil
+}
+
+func (fi *fakeIptables) DeleteIfExists(table, chain string, rulespec ...string) error {
+	m := fmt.Sprintf("DeleteIfExists(%s, %s, %v)", table, chain, rulespec)
+	fi.commandsExecuted = append(fi.commandsExecuted, m)
+	return nil
+}
+
+func (fi *fakeIptables) ClearAndDeleteChain(table, chain string) error {
+	m := fmt.Sprintf("ClearAndDeleteChain(%s, %s)", table, chain)
+	fi.commandsExecuted = append(fi.commandsExecuted, m)
+	return nil
+}
+
+func TestBlockIpv4(t *testing.T) {
+	v4 := &fakeIptables{}
+	v6 := &fakeIptables{}
+	b := &Blocker{ip4tables: v4, ip6tables: v6}
+	ip := net.ParseIP("127.0.0.1")
+	b.init()
+	b.Block(&ip)
+	b.Close()
+
+	wantv4 := []string{
+		"ChainExists(filter, contrackr)",
+		"NewChain(filter, contrackr)",
+		"Insert(filter, INPUT, 1, [-m state --state NEW -j contrackr])",
+		"AppendUnique(filter, contrackr, [-s 127.0.0.1 -j DROP])",
+		"DeleteIfExists(filter, INPUT, [-m state --state NEW -j contrackr])",
+		"ClearAndDeleteChain(filter, contrackr)",
+	}
+
+	wantv6 := []string{
+		"ChainExists(filter, contrackr)",
+		"NewChain(filter, contrackr)",
+		"Insert(filter, INPUT, 1, [-m state --state NEW -j contrackr])",
+		"DeleteIfExists(filter, INPUT, [-m state --state NEW -j contrackr])",
+		"ClearAndDeleteChain(filter, contrackr)",
+	}
+
+	if diff := cmp.Diff(wantv4, v4.commandsExecuted); diff != "" {
+		t.Errorf("Block() mismatch (-want +got):\n%s", diff)
+	}
+
+	if diff := cmp.Diff(wantv6, v6.commandsExecuted); diff != "" {
+		t.Errorf("Block() mismatch (-want +got):\n%s", diff)
+	}
+}
+
+func TestBlockIpv6(t *testing.T) {
+	v4 := &fakeIptables{}
+	v6 := &fakeIptables{}
+	b := &Blocker{ip4tables: v4, ip6tables: v6}
+	ip := net.ParseIP("2001:4860:4860::8888")
+	b.init()
+	b.Block(&ip)
+	b.Close()
+
+	wantv4 := []string{
+		"ChainExists(filter, contrackr)",
+		"NewChain(filter, contrackr)",
+		"Insert(filter, INPUT, 1, [-m state --state NEW -j contrackr])",
+		"DeleteIfExists(filter, INPUT, [-m state --state NEW -j contrackr])",
+		"ClearAndDeleteChain(filter, contrackr)",
+	}
+
+	wantv6 := []string{
+		"ChainExists(filter, contrackr)",
+		"NewChain(filter, contrackr)",
+		"Insert(filter, INPUT, 1, [-m state --state NEW -j contrackr])",
+		"AppendUnique(filter, contrackr, [-s 2001:4860:4860::8888 -j DROP])",
+		"DeleteIfExists(filter, INPUT, [-m state --state NEW -j contrackr])",
+		"ClearAndDeleteChain(filter, contrackr)",
+	}
+
+	if diff := cmp.Diff(wantv4, v4.commandsExecuted); diff != "" {
+		t.Errorf("Block() mismatch (-want +got):\n%s", diff)
+	}
+
+	if diff := cmp.Diff(wantv6, v6.commandsExecuted); diff != "" {
+		t.Errorf("Block() mismatch (-want +got):\n%s", diff)
+	}
+}
